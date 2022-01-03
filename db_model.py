2d37340312684856ac15786eb56d58ec2fef3da4
Colby Hillman <colby.hillman@gmail.com>
2022-01-02 21:23:00 -0500

    Linted db_model.py

--- db_model.py
+++ db_model.py
@@ -1,35 +1,38 @@
+"""Module to interact with a database"""
 import db_wrapper as db
 
-## Add channel
 def add_channel(channel_name, video_info):
+    """Adds a channel to the "channels" hash """
     db.add("channels", channel_name, video_info)
 
-## Remove channel
 def remove_channel(channel_name):
+    """Removes a channel from the "channels" hash"""
     db.remove("channels", channel_name)
 
 def list_channel_info(channel_name):
+    """Lists the info for a channel"""
     return db.list_field("channels", channel_name)
 
-## List channels
 def list_channels():
+    """Lists out all channels in the "channels" hash"""
     return db.list_hash("channels")
 
-## Add discord server
 def add_discord_server(server_id, subs):
+    """Adds a discord server to the "discord_servers" hash"""
     db.add("discord_servers", server_id, subs)
 
 def remove_discord_server(server_id):
+    """Removes a discord server from the "discord_servers" hash"""
     db.remove("discord_servers", server_id)
 
-## Add channel to server subs
 def add_channel_to_server_subs(server_id, channel_id):
+    """Adds a channel to a discord server"""
     db.add("discord_servers", server_id, channel_id)
 
-## Remove channel from server subs
 def remove_channel_from_server_subs(server_id, channel_id):
+    """Removes a channel from a discord server"""
     db.remove_field("discord_servers", server_id, channel_id)
 
-## List server subs
 def list_server_subs(server_id):
+    """Lists all the channels within a discord server"""
     return db.list_field("discord_servers", server_id)
