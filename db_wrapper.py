df0061cbd6e2c2cd5749f0a61793bae3e5e93077
Colby Hillman <colby.hillman@gmail.com>
2022-01-02 21:16:43 -0500

    Linted db_wrapper.py

--- db_wrapper.py
+++ db_wrapper.py
@@ -1,22 +1,24 @@
+"""Module for interacting with a Redis database"""
 import redis
 
 r = redis.Redis(host='redis', port=6379)
 
-## Add hashset
 def add(hashname, field, value):
+    """Adds a field:value pair to a hash"""
     r.hset(hashname, field, value)
 
-## Remove hashset field
 def remove(hashname, field):
+    """Removes field from a hash"""
     r.hdel(hashname, field)
 
 def remove_field(hashname, field, value):
+    """Removes a value from a field in a hash"""
     r.hdel(hashname, field, value)
 
-## List channels
 def list_hash(hashname):
+    """Lists al the fields/values in a hash"""
     return r.hgetall(hashname)
 
 def list_field(hashname, field):
+    """Lists the value of a field in a hash"""
     return r.hget(hashname, field)
-
