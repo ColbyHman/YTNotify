42744384aeedf317e1270816a3e15d9c5a403810
Colby Hillman <colby.hillman@gmail.com>
2022-02-26 21:33:01 -0500

    Refactored to fit with MongoDB

--- db_wrapper.py
+++ db_wrapper.py
@@ -1,24 +1,42 @@
-"""Module for interacting with a Redis database"""
-import redis
+"""MongoDB Wrapper"""
+from mongo import Connect
 
-r = redis.Redis(host='redis', port=6379)
+client = Connect.get_connection()
+db = client.ytnotify
 
-def add(hashname, field, value):
-    """Adds a field:value pair to a hash"""
-    r.hset(hashname, field, value)
+def list_collections():
+    """Returns a list of collections in a database"""
+    return db.list_collection_names()
 
-def remove(hashname, field):
-    """Removes field from a hash"""
-    r.hdel(hashname, field)
+def get_collection(collection_name):
+    """Retrieve a collection if it exists. Returns None if non-existent"""
+    if collection_name in list_collections():
+        return db[collection_name]
+    return None
 
-def remove_field(hashname, field, value):
-    """Removes a value from a field in a hash"""
-    r.hdel(hashname, field, value)
+def add_entry_to_collection(collection_name, entry):
+    """Adds entry to an existing collection """
+    collection = get_collection(collection_name)
+    if collection != None:
+        collection.insert_one(entry)
+    else:
+        print("Collection does not exist!")
 
-def list_hash(hashname):
-    """Lists al the fields/values in a hash"""
-    return r.hgetall(hashname)
+def search_collection(collection_name, query):
+    """Searches an existing collection"""
+    collection = get_collection(collection_name)
+    if collection != None:
+        return collection.find(query)
+    return None
 
-def list_field(hashname, field):
-    """Lists the value of a field in a hash"""
-    return r.hget(hashname, field)
+def remove_entry_from_collection(collection_name, query):
+    """Removes an entry from an existing collection"""
+    collection = get_collection(collection_name)
+    if collection != None:
+        collection.delete_one(query)
+
+def update_entry_in_collection(collection_name, search, query):
+    """Updates an entry in an existing collection"""
+    collection = get_collection(collection_name)
+    if collection != None:
+        collection.update_one(search, {"$set":query})
