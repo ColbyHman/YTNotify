3fe00e4cac6bdff1f0c6663662ae884d3de6c3e9
Colby Hillman <colby.hillman@gmail.com>
2022-02-26 21:32:54 -0500

    Refactored to fit with MongoDB

--- db_model.py
+++ db_model.py
@@ -1,38 +1,41 @@
 """Module to interact with a database"""
 import db_wrapper as db
 
-def add_channel(channel_name, video_info):
-    """Adds a channel to the "channels" hash """
-    db.add("channels", channel_name, video_info)
+def add_channel(channel_info):
+    """Adds a channel"""
+    db.add_entry_to_collection("channels", channel_info)
 
-def remove_channel(channel_name):
-    """Removes a channel from the "channels" hash"""
-    db.remove("channels", channel_name)
+def update_channel(channel_info):
+    """Updates a channel"""
+    db.update_entry_in_collection("channels", channel_info)
 
-def list_channel_info(channel_name):
+def remove_channel(channel_info):
+    """Removes a channel"""
+    db.remove_entry_from_collection("channels", channel_info)
+
+def list_channel_info(channel_info):
     """Lists the info for a channel"""
-    return db.list_field("channels", channel_name)
+    return db.search_collection("channels", channel_info)[0]
 
 def list_channels():
-    """Lists out all channels in the "channels" hash"""
-    return db.list_hash("channels")
+    """Lists out all channels"""
+    return db.search_collection("channels", {})
 
-def add_discord_server(server_id, subs):
-    """Adds a discord server to the "discord_servers" hash"""
-    db.add("discord_servers", server_id, subs)
+def add_discord_server(server_info):
+    """Adds a discord server"""
+    db.add_entry_to_collection("discord_servers", server_info)
 
-def remove_discord_server(server_id):
-    """Removes a discord server from the "discord_servers" hash"""
-    db.remove("discord_servers", server_id)
+def remove_discord_server(server_info):
+    """Removes a discord server"""
+    db.remove_entry_from_collection("discord_servers", server_info)
 
-def add_channel_to_server_subs(server_id, channel_id):
-    """Adds a channel to a discord server"""
-    db.add("discord_servers", server_id, channel_id)
+def update_discord_server(query, server_info):
+    """Updates a discord server"""
+    db.update_entry_in_collection("discord_servers", query, server_info)
 
-def remove_channel_from_server_subs(server_id, channel_id):
-    """Removes a channel from a discord server"""
-    db.remove_field("discord_servers", server_id, channel_id)
+def list_server_info(server_info):
+    """Lists discord server info"""
+    return db.search_collection("discord_servers", server_info)[0]
 
-def list_server_subs(server_id):
-    """Lists all the channels within a discord server"""
-    return db.list_field("discord_servers", server_id)
+def list_servers():
+    return db.search_collection("discord_servers", {})
