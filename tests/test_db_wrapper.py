c12673c012980ef4950106d43b933a7ff0ed7228
Colby Hillman <colby.hillman@gmail.com>
2022-03-03 22:29:00 -0500

    Added fixture, refactored wrapper reference

--- tests/test_db_wrapper.py
+++ tests/test_db_wrapper.py
@@ -1,6 +1,8 @@
+import pytest
 import sys
 sys.path.append('application/database')
-import db_wrapper as db
+import db_wrapper as wrapper
+
 
 test_entry = {"name":"testChannel",
             "video_id":"video",
@@ -18,57 +20,62 @@ test_entry_updated = {"name":"testChannelUpdated",
                     "last_updated":"now"
                     }
 
-def test_list_collections():
-    collections = db.list_collections()
+@pytest.fixture(scope="function")
+def setup():
+    wrapper.remove_all_entries_from_collection("discord_servers")
+    wrapper.remove_all_entries_from_collection("channels")
+
+def test_list_collections(setup):
+    collections = wrapper.list_collections()
     assert "discord_servers" in collections and "channels" in collections
 
-def test_get_collections():
-    ds_collection = db.get_collection("discord_servers")
-    c_collection = db.get_collection("channels")
+def test_get_collections(setup):
+    ds_collection = wrapper.get_collection("discord_servers")
+    c_collection = wrapper.get_collection("channels")
     assert ds_collection.name == "discord_servers" and c_collection.name == "channels"
 
-def test_add_entry_to_collection():
+def test_add_entry_to_collection(setup):
     try:
-        db.add_entry_to_collection("channels",test_entry)
-        query = db.search_collection_for_one("channels",{"name":"testChannel"})
+        wrapper.add_entry_to_collection("channels",test_entry)
+        query = wrapper.search_collection_for_one("channels",{"name":"testChannel"})
         assert query["name"] == "testChannel"
     except Exception as exec:
         assert False, exec
-    db.remove_entry_from_collection("channels",test_entry)
+    wrapper.remove_entry_from_collection("channels",test_entry)
 
-def test_remove_entry_from_collection():
-    db.add_entry_to_collection("channels",test_entry)
+def test_remove_entry_from_collection(setup):
+    wrapper.add_entry_to_collection("channels",test_entry)
     try:
-        db.remove_entry_from_collection("channels",test_entry)
-        query = db.search_collection_for_one("channels",{"name":"testChannel"})
+        wrapper.remove_entry_from_collection("channels",test_entry)
+        query = wrapper.search_collection_for_one("channels",{"name":"testChannel"})
         assert query is None
     except Exception as exec:
         assert False, exec
 
-def test_search_collection():
-    db.add_entry_to_collection("channels",test_entry)
+def test_search_collection(setup):
+    wrapper.add_entry_to_collection("channels",test_entry)
     try:
-        query = db.search_collection("channels", {"name":"testChannel"})
-        db.remove_entry_from_collection("channels",test_entry)
+        query = wrapper.search_collection("channels", {"name":"testChannel"})
+        wrapper.remove_entry_from_collection("channels",test_entry)
         assert query is not None
     except Exception as exec:
         assert False, exec
 
-def test_search_collection_for_one():
-    db.add_entry_to_collection("channels",test_entry)
+def test_search_collection_for_one(setup):
+    wrapper.add_entry_to_collection("channels",test_entry)
     try:
-        query = db.search_collection_for_one("channels", {"name":"testChannel"})
-        db.remove_entry_from_collection("channels",{"name":"testChannel"})
+        query = wrapper.search_collection_for_one("channels", {"name":"testChannel"})
+        wrapper.remove_entry_from_collection("channels",{"name":"testChannel"})
         assert query["name"] == "testChannel"
     except Exception as exec:
         assert False, exec
 
-def test_update_entry_in_collection():
-    db.add_entry_to_collection("channels",test_entry)
+def test_update_entry_in_collection(setup):
+    wrapper.add_entry_to_collection("channels",test_entry)
     try:
-        db.update_entry_in_collection("channels", {"name":"testChannel"}, test_entry_updated)
-        query = db.search_collection_for_one("channels", {"name":"testChannelUpdated"})
-        db.remove_entry_from_collection("channels",{"name":"testChannelUpdated"})
+        wrapper.update_entry_in_collection("channels", {"name":"testChannel"}, test_entry_updated)
+        query = wrapper.search_collection_for_one("channels", {"name":"testChannelUpdated"})
+        wrapper.remove_entry_from_collection("channels",{"name":"testChannelUpdated"})
         assert query["name"] == "testChannelUpdated"
     except Exception as exec:
         assert False, exec
