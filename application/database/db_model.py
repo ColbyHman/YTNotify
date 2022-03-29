ec1c18eeb001f5b5ecd34e6e692429b0443a3aa1
Colby Hillman <colby.hillman@gmail.com>
2022-03-29 14:05:57 -0400

    Added error handling for DB model

--- application/database/db_model.py
+++ application/database/db_model.py
@@ -3,40 +3,71 @@ import db_wrapper as db
 
 def add_channel(channel_info):
     """Adds a channel"""
-    db.add_entry_to_collection("channels", channel_info)
+    try:
+        db.add_entry_to_collection("channels", channel_info)
+    except:
+        raise Exception("Database Unavailable")
 
 def update_channel(search, channel_info):
     """Updates a channel"""
-    db.update_entry_in_collection("channels", search, channel_info)
+    try:
+        db.update_entry_in_collection("channels", search, channel_info)
+    except:
+        raise Exception("Database Unavailable")
 
 def remove_channel(channel_info):
     """Removes a channel"""
-    db.remove_entry_from_collection("channels", channel_info)
+    try:
+        db.remove_entry_from_collection("channels", channel_info)
+    except:
+        raise Exception("Database Unavailable")
 
 def list_channel_info(channel_info):
     """Lists the info for a channel"""
-    return db.search_collection("channels", channel_info)[0]
+    try:
+        return db.search_collection("channels", channel_info)
+    except:
+        raise Exception("Database Unavailable")
 
 def list_channels():
     """Lists out all channels"""
-    return db.search_collection("channels", {})
+    try:
+        return db.search_collection("channels", {})
+    except:
+        raise Exception("Database Unavailable")
 
 def add_discord_server(server_info):
     """Adds a discord server"""
-    db.add_entry_to_collection("discord_servers", server_info)
+    try:
+        db.add_entry_to_collection("discord_servers", server_info)
+    except:
+        raise Exception("Database Unavailable")
 
 def remove_discord_server(server_info):
     """Removes a discord server"""
-    db.remove_entry_from_collection("discord_servers", server_info)
+    try:
+        db.remove_entry_from_collection("discord_servers", server_info)
+    except:
+        raise Exception("Database Unavailable")
 
 def update_discord_server(query, server_info):
     """Updates a discord server"""
-    db.update_entry_in_collection("discord_servers", query, server_info)
+    try:
+        db.update_entry_in_collection("discord_servers", query, server_info)
+    except:
+        raise Exception("Database Unavailable")
 
 def list_server_info(server_info):
     """Lists discord server info"""
-    return db.search_collection("discord_servers", server_info)[0]
+    try:
+        return db.search_collection("discord_servers", server_info)
+    except:
+        raise Exception("Database Unavailable")
 
 def list_servers():
     """Lists all discord servers"""
-    return db.search_collection("discord_servers", {})
+    try:
+        return db.search_collection("discord_servers", {})
+    except:
+        raise Exception("Database Unavailable")
+        
